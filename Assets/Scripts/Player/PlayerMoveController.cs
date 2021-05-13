2598f4f29608d4f181ceed58e285b1a5bf9bb98b
nictrak <narin14175@gmail.com>
2021-05-13 20:18:47 +0700

    Camera following and player basic move complete

---
+++ Assets/Scripts/Player/PlayerMoveController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveVelocity;
+
+    // Calculation field
+    private Vector2 moveDirection;
+
+    // In same object field
+    private Rigidbody2D rgbody;
+
+    // Action permission field
+    private bool canMove;
+    private bool canUpdateMoveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbody = GetComponent<Rigidbody2D>();
+        canMove = true;
+        canUpdateMoveDirection = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canUpdateMoveDirection) UpdateMoveDirection();
+    }
+    // Fixed frame update
+    private void FixedUpdate()
+    {
+        if (canMove) MovePerFrame();
+    }
+
+    // Start move method
+    private Vector2 GetInputMoveDirection()
+    {
+        Vector2 res = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        return res.normalized;
+    }
+    private void UpdateMoveDirection()
+    {
+        moveDirection = GetInputMoveDirection();
+    }
+    private Vector2 CalMoveVector()
+    {
+        return moveDirection * moveVelocity;
+    }
+    private void Move(Vector2 moveVector)
+    {
+        Vector2 newPosition = rgbody.position + moveVector;
+        rgbody.MovePosition(newPosition);
+    }
+    private void MovePerFrame()
+    {
+        Move(CalMoveVector());
+    }
+    // End of move method
+}
---
+++ Assets/Scripts/System/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followed;
+    public Vector3 translate;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        transform.position = followed.position + translate;
+    }
+}
