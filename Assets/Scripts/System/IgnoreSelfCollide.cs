61828426dcc98f8ed6e35f702a47ba7a38e5412d
nictrak <narin14175@gmail.com>
2021-05-24 19:47:21 +0700

    Collider block complete

---
+++ Assets/Scripts/System/IgnoreSelfCollide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreSelfCollide : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D collider1;
+    [SerializeField]
+    private Collider2D collider2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(collider1, collider2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
