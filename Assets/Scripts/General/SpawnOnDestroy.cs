1d42f9b7098d45859acc7d1ff6a6dcf2647a7589
Kasidit Ratanawongpitak <kasidit_128@hotmail.com>
2021-05-29 03:02:31 +0700

    Add Movement & Debuff & OnHit & Modified Player Controller to Trigger Debuff

---
+++ Assets/Scripts/General/SpawnOnDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnDestroy : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        GameObject spawned =  Instantiate(gameObject);
+        spawned.transform.position = transform.position;
+
+    }
+    
+}
