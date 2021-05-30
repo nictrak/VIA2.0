e13d2eb3a5b96e508f923c6a6e3ede0f4d9032cd
nictrak <narin14175@gmail.com>
2021-05-30 13:56:22 +0700

    DialogueController complete

---
+++ Assets/Scripts/UserInterface/DialogueController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dialoguePanel;
+    [SerializeField]
+    private Text talkerNameText;
+    [SerializeField]
+    private Text dialogueText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetText(string talkerName, string dialogue)
+    {
+        talkerNameText.text = talkerName;
+        dialogueText.text = dialogue;
+    }
+    public void SetActivePanel(bool isActive)
+    {
+        dialoguePanel.SetActive(isActive);
+    }
+}
