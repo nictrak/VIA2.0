28e365934fbbfdcc82bdf107fc7891f7c5c07e59
Kasidit Ratanawongpitak <kasidit_128@hotmail.com>
2021-06-01 04:06:07 +0700

    Add Inventory System (Equipment Panel, Inventory Panel, Drag Drop)

---
+++ Assets/Scripts/Inventory/EquipmentPanel.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System;
+
+public class EquipmentPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Transform equipmentSlotsParent;
+    [SerializeField]
+    private EquipmentSlot[] equipmentSlots;
+
+    public event Action<ItemSlot> OnRightClickEvent;
+    public event Action<ItemSlot> OnBeginDragEvent;
+    public event Action<ItemSlot> OnEndDragEvent;
+    public event Action<ItemSlot> OnDragEvent;
+    public event Action<ItemSlot> OnDropEvent;
+
+    private void Start() {
+        for ( int i = 0 ; i < equipmentSlots.Length ; i++ )
+        {
+            equipmentSlots[i].OnRightClickEvent += OnRightClickEvent;
+            equipmentSlots[i].OnBeginDragEvent += OnBeginDragEvent;
+            equipmentSlots[i].OnEndDragEvent += OnEndDragEvent;
+            equipmentSlots[i].OnDragEvent += OnDragEvent;
+            equipmentSlots[i].OnDropEvent += OnDropEvent;
+        }
+    }
+
+    private void OnValidate()
+    {
+        equipmentSlots = equipmentSlotsParent.GetComponentsInChildren<EquipmentSlot>();
+    }
+
+    public bool AddItem(EquippableItem item, out EquippableItem previousItem)
+    {
+        for (int i =0; i< equipmentSlots.Length ; i++)
+        {
+            if(equipmentSlots[i].EquipmentType == item.EquipmentType)
+            {
+                previousItem = (EquippableItem) equipmentSlots[i].Item;
+                equipmentSlots[i].Item = item;
+                return true;
+            }
+        }
+        previousItem = null;
+        return false;
+    }
+
+    public bool RemoveItem(EquippableItem item)
+    {
+        for (int i =0; i< equipmentSlots.Length ; i++)
+        {
+            if(equipmentSlots[i].Item == item)
+            {
+                equipmentSlots[i].Item = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/Inventory/EquippableItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public enum EquipmentType
+{
+    Weapon,
+    Armour
+}
+
+[CreateAssetMenu(fileName = "New Equippable Item", menuName = "VIA2.0/Equippable Item", order = 0)]
+public class EquippableItem : Item
+{
+    public EquipmentType EquipmentType;
+}
