ae32b08e118c3582141f4e8c6c0f526bdf6da1b0
nictrak <narin14175@gmail.com>
2021-06-05 15:32:49 +0700

    Can ignore macthed modifier complete

--- Assets/Scripts/Inventory/Weapon/Weapon.cs
+++ Assets/Scripts/Inventory/Weapon/Weapon.cs
@@ -6,18 +6,37 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField]
     private List<Modifier> modifiersPrefab;
-
+    [SerializeField]
+    private List<string> ignoredModifiers;
     public List<Modifier> ModifiersPrefab { get => modifiersPrefab; set => modifiersPrefab = value; }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ignoredModifiers = new List<string>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ClearAllNull();
+        UpdateIgnoredModifiers();
+    }
+    private void UpdateIgnoredModifiers()
+    {
+        List<string> newIgnore = new List<string>();
+        for (int i = 0; i < modifiersPrefab.Count; i++)
+        {
+            newIgnore.Add(modifiersPrefab[i].name);
+        }
+        ignoredModifiers = newIgnore;
+    }
+    public void ClearAllNull()
+    {
+        ModifiersPrefab.RemoveAll(mod => mod == null);
+    }
+    public bool IsCanAdd(Modifier newMod)
+    {
+        return !ignoredModifiers.Contains(newMod.name);
     }
 }
