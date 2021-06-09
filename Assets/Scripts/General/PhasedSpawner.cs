eba96900335caa950c545c284c0dc235e6e2cc9d
nictrak <narin14175@gmail.com>
2021-06-10 03:46:31 +0700

    Phase spawner complete

--- Assets/Scripts/General/PhasedSpawner.cs
+++ Assets/Scripts/General/PhasedSpawner.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class PhasedSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> enemies;
 
+    private int checkFrame;
+    private int counter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkFrame = 100;
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        if (enemies.Count > 0)
+        {
+            if(counter >= checkFrame)
+            {
+                if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+                {
+                    GameObject spawned = Instantiate(enemies[0]);
+                    spawned.transform.position = transform.position;
+                    enemies.RemoveAt(0);
+                    counter = 0;
+                }
+            }
+            else
+            {
+                counter++;
+            }
+        }
     }
 }
