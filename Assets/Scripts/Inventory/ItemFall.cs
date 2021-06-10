daa1f71845efbd2f04deb43e2f292bb9365e29b8
nictrak <narin14175@gmail.com>
2021-06-11 00:19:30 +0700

    Add Item fall

---
+++ Assets/Scripts/Inventory/ItemFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFall : MonoBehaviour
+{
+    [SerializeField]
+    private Item item;
+
+    public Item Item { get => item; set => item = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
