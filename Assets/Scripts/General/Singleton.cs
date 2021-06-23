ba40bdc061036148452ca36eba743db4d5831606
nictrak <narin14175@gmail.com>
2021-06-23 18:07:12 +0700

    Add singleton

---
+++ Assets/Scripts/General/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    [SerializeField]
+    private string currentHash;
+    [SerializeField]
+    private bool isDontDestroyOnLoad;
+    private static Dictionary<string, GameObject> singletons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (singletons == null)
+        {
+            singletons = new Dictionary<string, GameObject>();
+        }
+        if (singletons.ContainsKey(currentHash))
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            singletons.Add(currentHash, gameObject);
+        }
+        if(isDontDestroyOnLoad) DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
