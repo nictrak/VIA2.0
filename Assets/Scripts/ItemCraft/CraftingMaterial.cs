20bf1eac9b015bcb7da77115fbe3405cf86ef557
nictrak <narin14175@gmail.com>
2021-06-30 03:45:43 +0700

    Simple crafting gravity complete

---
+++ Assets/Scripts/ItemCraft/CraftingMaterial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingMaterial : MonoBehaviour
+{
+    [SerializeField]
+    private CraftingMaterialItem itemData;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnValidate()
+    {
+        if(spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        spriteRenderer.sprite = itemData.Icon;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+    public string GetItemName()
+    {
+        return itemData.Name;
+    }
+}
---
+++ Assets/Scripts/ItemCraft/ItemCraftGravity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCraftGravity : MonoBehaviour
+{
+    [SerializeField]
+    private float gravityVelocity;
+    private Rigidbody2D rgbody;
+    private TriggerRange triggerRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerRange = GetComponentInChildren<TriggerRange>();
+        rgbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+               
+    }
+    private void FixedUpdate()
+    {
+        rgbody.MovePosition(rgbody.position + CalMoveVector());
+    }
+    private Vector2 CalSingleMoveVector(Vector2 otherPosition)
+    {
+        return (otherPosition - (Vector2)transform.position).normalized * gravityVelocity;
+    }
+    public Vector2 CalMoveVector()
+    {
+        Vector2 result = new Vector2();
+        for(int i = 0; i < triggerRange.Objs.Count; i++)
+        {
+            result = result + CalSingleMoveVector(triggerRange.Objs[i].transform.position);
+        }
+        return result;
+    }
+}
