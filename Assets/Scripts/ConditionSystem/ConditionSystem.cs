7d6b7960481b67639658a5327669d3c4ac2249e7
nictrak <narin14175@gmail.com>
2021-07-19 08:04:26 +0700

    Add condition system

---
+++ Assets/Scripts/ConditionSystem/ConditionSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionSystem : MonoBehaviour
+{
+    public static Dictionary<string, bool> ConditionHash;
+    private List<string> conditionTopics;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(ConditionHash == null)
+        {
+            ConditionHash = new Dictionary<string, bool>();
+            for(int i = 0; i < conditionTopics.Count; i++)
+            {
+                ConditionHash.Add(conditionTopics[i], false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
