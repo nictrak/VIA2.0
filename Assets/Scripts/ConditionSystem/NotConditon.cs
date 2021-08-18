74ce27668919eaaf4e45910995ce797582cc0936
nictrak <narin14175@gmail.com>
2021-08-18 16:49:05 +0700

    Add not condition

---
+++ Assets/Scripts/ConditionSystem/NotConditon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Not Condition", menuName = "VIA2.0/Condition/Not Condition", order = 0)]
+public class NotConditon : Condition
+{
+    [SerializeField]
+    private string conditionTopic;
+
+    public string ConditionTopic { get => conditionTopic; set => conditionTopic = value; }
+
+    public override bool IsPass()
+    {
+        if (ConditionSystem.GetCondition(conditionTopic)) return false;
+        return true;
+    }
+}
