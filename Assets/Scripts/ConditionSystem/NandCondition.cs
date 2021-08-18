b56fca9eda30f8515d45563998ebec66b15b7d7d
nictrak <narin14175@gmail.com>
2021-08-18 17:38:20 +0700

    Add new condition

---
+++ Assets/Scripts/ConditionSystem/NandCondition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Nand Condition", menuName = "VIA2.0/Condition/Nand Condition", order = 0)]
+public class NandCondition : Condition
+{
+    [SerializeField]
+    private List<string> conditionTopics;
+    public override bool IsPass()
+    {
+        for (int i = 0; i < conditionTopics.Count; i++)
+        {
+            if (!ConditionSystem.GetCondition(conditionTopics[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
