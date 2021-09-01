0158ac23d56933f667577cd5abf84d1818d0d2a8
nictrak <narin14175@gmail.com>
2021-09-01 20:14:20 +0700

    Add TimeSystem

---
+++ Assets/Scripts/System/TimeSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSystem : MonoBehaviour
+{
+    public float slowdownFactor;
+    private bool isSlow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSlow = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (!isSlow)
+            {
+                DoSlowMotion();
+            }
+        }
+        else
+        {
+            if (isSlow)
+            {
+                DoStandardTime();
+            }
+        }
+    }
+    public void DoSlowMotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        isSlow = true;
+    }
+    public void DoStandardTime()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        isSlow = false;
+    }
+}
