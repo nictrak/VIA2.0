e61803c487791b51256195dde5df47d551564a82
waritphon S <35068600+Waritphon@users.noreply.github.com>
2021-10-13 15:39:00 +0700

    Add WarpDialog

---
+++ Assets/Scripts/NPC/DialogueSingleNextWrap.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSingleNextWrap : Dialogue
+{
+    [SerializeField]
+    private Dialogue next;
+    [SerializeField]
+    private string nextLevel;
+    [SerializeField]
+    private Condition condition;
+    [SerializeField]
+    private bool IsPassIfTrue;
+
+    public override void EndDialogue()
+    {
+        if (condition.IsPass() == IsPassIfTrue)
+        {
+            WarpSystem.WarpToScene(nextLevel);
+        }
+    }
+
+    public override List<string> GetChoicesString()
+    {
+        return null;
+    }
+
+    public override Dialogue GetNextDialogue(int param)
+    {
+        return next;
+    }
+
+    public override bool IsClicknext()
+    {
+        return true;
+    }
+
+    public override void StartDialogue()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
