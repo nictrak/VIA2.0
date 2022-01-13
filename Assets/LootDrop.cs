e4a93773d56d1ab7c3f1e94b0f75bfe9e60b55d1
waritphon S <35068600+Waritphon@users.noreply.github.com>
2022-01-13 23:26:48 +0700

    Add monster Die item pop
    Add more
    - Item splash when dir
    - Cloud pop
    - random splash range
    - Drop multi item
    - random drop

--- Assets/LootDrop.cs
+++ Assets/LootDrop.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LootDrop : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private GameObject itemCraftPrefab;
+    [SerializeField]
+    private Item itemScript;
+    
     void Start()
     {
         
@@ -14,5 +19,23 @@ public class LootDrop : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnDestroy() {
+        // Use same code base as PlayerShortcutController
+        GameObject spawned = Instantiate(itemCraftPrefab);
+        
+        spawned.transform.position = transform.position;
+        CraftingMaterial craftingMaterial = spawned.GetComponent<CraftingMaterial>();
+        craftingMaterial.ItemData = (CraftingMaterialItem)itemScript.Copy();
+
+        ItemPickable itemPickable = spawned.GetComponent<ItemPickable>();
+        itemPickable.Item = itemScript.Copy();
+
+        Animation itemAnimation = spawned.GetComponent<Animation>();
+        // Debug.Log(itemScript.name);
+        itemAnimation.Play(itemScript.name);
+        // Vector3 pos = new Vector3(Random.Range(-1,1),Random.Range(-1,1),0);
+        // Instantiate(dropOnDamaged, dropOnDamaged.position);
     }
 }
