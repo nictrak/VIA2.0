7d93c5d5060f5543e2edfbbfa1966611a16c2c3c
Kasidit <6030031021@student.chula.ac.th>
2022-02-25 15:01:42 +0700

    Update Aiming System
    - Move Prefab from TargetBuffer to GeneralAimState
    - Add AimingTime ( Time when trigger aim )
    - Add WaitingTime ( Time waiting before entering next state )
    - Fix AimPrefab remaining after deathState

--- Assets/Scripts/Monster/State/GeneralAimState.cs
+++ Assets/Scripts/Monster/State/GeneralAimState.cs
@@ -5,28 +5,48 @@ using Pathfinding;
 
 public class GeneralAimState : MonsterStateBehaviour
 {
+    [SerializeField]
+    private GameObject aimPrefab;
+    
+    [SerializeField]
+    private int aimingTime;
+    [SerializeField]
+    private int waitingTime;
+    private int waitingCounter;
+    private bool isAimed;
     private TargetBuffer targetBuffer;
     public override void ExitState()
     {
-        
+
     }
 
     public override MonsterStateMachine.MonsterState RunState()
     {
-        targetBuffer.TargetPosition = GetTarget().transform.position;
-        targetBuffer.IsAim = true;
-        return NormalNextState;
+        if(waitingCounter >= aimingTime && !isAimed) {
+            Vector2 position = GetTarget().transform.position;
+            targetBuffer.Aim(aimPrefab, position);
+            isAimed = true;
+        }
+
+        if (waitingCounter >= waitingTime)
+        {
+            return NormalNextState;
+        }
+        else waitingCounter++;
+
+        return currentState;
     }
 
     public override void StartState()
     {
-        
+        waitingCounter = 0;
+        isAimed = false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        targetBuffer = GetComponentInParent<TargetBuffer>();
+        targetBuffer = this.transform.root.gameObject.GetComponent<TargetBuffer>();
     }
 
     // Update is called once per frame
